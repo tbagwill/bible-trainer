fc40cae8b95e4980e5063f313cb7d487a10d5103
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-08-31 01:35:17 -0700

    added memory verse! (early demo)

--- src/lib/data/memory.js
+++ src/lib/data/memory.js
@@ -1,22 +1,57 @@
+export function randomMemoryVerse() {
+	const size = memoryVerses.length;
+	const res = Math.floor(Math.random() * size);
+	return res;
+}
+
 export const memoryVerses = [
 	{
 		question: 'For God so loved the world...',
 		answers: [
 			{
 				response: '...He gave his only son..',
-				correct: true
+				correct: true,
+				status: 'success'
 			},
 			{
 				response: '...He flooded the Earth..',
-				correct: false
+				correct: false,
+				status: 'error'
 			},
 			{
 				response: '...He cast out the sins of the world..',
-				correct: false
+				correct: false,
+				status: 'error'
 			},
 			{
 				response: '...He took the men out of the desert..',
-				correct: false
+				correct: false,
+				status: 'error'
+			}
+		]
+	},
+	{
+		question: 'What sign did God show to remind us He will never flood the Earth again?',
+		answers: [
+			{
+				response: 'The Rainbow',
+				correct: true,
+				status: 'success'
+			},
+			{
+				response: 'A donkey',
+				correct: false,
+				status: 'error'
+			},
+			{
+				response: 'His son, Jesus',
+				correct: false,
+				status: 'error'
+			},
+			{
+				response: 'Happy Hour',
+				correct: false,
+				status: 'error'
 			}
 		]
 	}
--- src/routes/home/learn/memory/+page.svelte
+++ src/routes/home/learn/memory/+page.svelte
@@ -1,18 +1,179 @@
 <script>
-	import { memoryVerses } from '$lib/data/memory';
+	import { memoryVerses, randomMemoryVerse } from '$lib/data/memory';
+	import { ProgressBar, RangeSlider, RadioGroup } from '@skeletonlabs/skeleton';
+	import { fade, fly, scale } from 'svelte/transition';
+
+	let GAME_LENGTH = 5;
+	let DIFFICULTY = 1;
+	let POINTS = 0;
+	let INCREMENT = 10 * DIFFICULTY;
+	$: MAX_POINTS = GAME_LENGTH * DIFFICULTY * 10;
+
+	let GUESSES = 0;
+
+	let CURR_QUESTION = memoryVerses[randomMemoryVerse()];
+	let ANSWER_HIDDEN = true;
+	let ANSWER_DISABLED = false;
+
+	$: console.log(POINTS);
+
+	let GAME_STATE = ['READY', 'PLAYING', 'FINISHED'];
+	let STAGE_STATE = ['UNANSWERED', 'ANSWERED'];
+
+	let CURR_GAME_STATE = GAME_STATE[0];
+	let CURR_STAGE_STATE = STAGE_STATE[0];
+
+	function makeGuess(guess, index) {
+		//  Animations?
+		const boxes = document.getElementsByClassName('guess-box');
+		// Declare guess was made
+		GUESSES++;
+		CURR_STAGE_STATE = STAGE_STATE[1];
+		ANSWER_HIDDEN = false;
+		ANSWER_DISABLED = true;
+		// Check response
+		if (guess.correct) {
+			POINTS += INCREMENT;
+		} else {
+			// FAIL()
+		}
+		// Reset after 2.5 seconds
+		setTimeout(() => {
+			ANSWER_HIDDEN = true;
+			ANSWER_DISABLED = false;
+			CURR_STAGE_STATE = STAGE_STATE[0];
+			CURR_QUESTION = memoryVerses[randomMemoryVerse()];
+		}, '2500');
+	}
+
+	function resetGame() {
+		setTimeout(() => {
+			CURR_GAME_STATE = GAME_STATE[0];
+			CURR_QUESTION = memoryVerses[randomMemoryVerse()];
+			CURR_STAGE_STATE = STAGE_STATE[0];
+			ANSWER_HIDDEN = true;
+			ANSWER_DISABLED = false;
+			GUESSES = 0;
+			POINTS = 0;
+		}, '100');
+	}
+
+	function resetCurrentGame() {
+		setTimeout(() => {
+			CURR_GAME_STATE = GAME_STATE[1];
+			CURR_QUESTION = memoryVerses[randomMemoryVerse()];
+			CURR_STAGE_STATE = STAGE_STATE[0];
+			ANSWER_HIDDEN = true;
+			ANSWER_DISABLED = false;
+			POINTS = 0;
+			GUESSES = 0;
+		}, '100');
+	}
+
+	$: if (GUESSES >= GAME_LENGTH)
+		setTimeout(() => {
+			CURR_GAME_STATE = GAME_STATE[2];
+		}, '2500');
+	$: format = (status) => (ANSWER_HIDDEN ? 'variant-filled-primary ' : `variant-filled-${status}`);
+	$: disabled = ANSWER_DISABLED ? 'disabled' : '';
 </script>
 
-<div class="flex flex-col w-full p-4">
-	<div class="card variant-ringed-primary text-xl font-bold p-4 mb-8">
-		{memoryVerses[0].question}
+{#if CURR_GAME_STATE === 'READY'}
+	<!-- READY SCREEN -->
+	<div class="w-full space-y-4 p-4" in:fly={{ x: -120, delay: 100 }}>
+		<h3 class="w-full text-3xl font-bold">Memory Verses</h3>
+		<hr class="w-full bg-surface-700" />
 	</div>
-	<div class="flex flex-col justify-center items-center space-y-4">
-		{#each memoryVerses[0].answers as answer}
-			<button class="btn variant-filled-primary rounded-lg w-11/12 md:w-3/4 text-lg font-semibold">
-				<h3 class="whitespace-pre-wrap">
-					{answer.response}
-				</h3>
+	<div class="flex flex-col w-full h-1/2 items-center justify-center space-y-4">
+		<div
+			class="card flex flex-col w-11/12 md:w-3/4 p-4 items-center space-y-4"
+			in:scale={{ delay: 100 }}
+		>
+			<h3 class="w-full text-3xl text-left font-semibold">Game Settings</h3>
+			<br />
+			<div class="flex w-full align-bottom items-end">
+				<h3 class="w-full text-xl text-left font-semibold">Number of Questions</h3>
+				<span class="label w-full text-right text-md font-semibold">{GAME_LENGTH} / 25</span>
+			</div>
+			<div class="w-full">
+				<RangeSlider name="game-length" min="1" max="25" bind:value={GAME_LENGTH} />
+			</div>
+
+			<button
+				class="btn variant-filled-primary w-1/2"
+				on:click={() => (CURR_GAME_STATE = GAME_STATE[1])}
+			>
+				Let's Play
 			</button>
-		{/each}
+		</div>
 	</div>
-</div>
+
+	<!-- PLAYING THE GAME -->
+{:else if CURR_GAME_STATE === 'PLAYING'}
+	<ProgressBar
+		label="Progress Bar"
+		meter="bg-gradient-to-r variant-gradient-primary-secondary"
+		track="bg-surface-500"
+		value={GUESSES}
+		max={GAME_LENGTH}
+		rounded="rounded-r-full"
+	/>
+	<div class="flex flex-col w-full p-4">
+		<div class="card variant-ringed-primary text-xl font-bold p-4 mb-8" in:fade={{ delay: 100 }}>
+			{CURR_QUESTION.question}
+		</div>
+		<div class="flex flex-col justify-center items-center space-y-4" in:scale={{ delay: 100 }}>
+			{#each CURR_QUESTION.answers as answer, count}
+				<button
+					class="btn {format(
+						answer.status
+					)} rounded-lg w-11/12 md:w-3/4 text-lg font-semibold guess-box"
+					value={count}
+					{disabled}
+					on:click={() => makeGuess(answer, count)}
+				>
+					<h3 class="whitespace-pre-wrap">
+						{answer.response}
+					</h3>
+				</button>
+			{/each}
+		</div>
+	</div>
+{:else if CURR_GAME_STATE === 'FINISHED'}
+	<!-- modal -->
+	<div class="w-full flex justify-center p-4">
+		<div class="card flex flex-col w-11/12 md:w-3/4 p-4 md:p-6" in:scale={{ delay: 100 }}>
+			<div class="w-full text-3xl text-center font-bold">Game Over</div>
+			<hr class="bg-primary-500 rounded-full p-0.5 mt-2 mb-2" />
+			<div class="w-full text-xl text-center font-bold">Score: {POINTS} / {MAX_POINTS}</div>
+			<br />
+			{#if POINTS < MAX_POINTS / 2}
+				<div class="w-full text-xl text-center font-bold">
+					Oh I'm sure you can do better than that!
+				</div>
+			{:else if POINTS === MAX_POINTS}
+				<div class="w-full text-xl text-center font-bold">
+					Wow! You really know your stuff! Try some harder questions!
+				</div>
+			{:else}
+				<div class="w-full text-xl text-center font-bold">
+					Nice one! You did great! Think you can do better?
+				</div>
+			{/if}
+			<br />
+			<div class="flex flex-row w-full justify-evenly px-4 space-x-4">
+				<button
+					class="btn variant-outline-tertiary w-full text-xl text-center font-bold p-4"
+					on:click={() => resetGame()}>Game Settings</button
+				>
+				<button
+					class="btn variant-filled-secondary w-full text-xl text-center font-bold p-4"
+					on:click={() => resetCurrentGame()}>Play Again</button
+				>
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style>
+</style>
